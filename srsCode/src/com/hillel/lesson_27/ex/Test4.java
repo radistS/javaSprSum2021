c588ef574555da44d41f7b4dd825428ee9df126b
alex <stepurko.alexandr@gmail.com>
2021-07-05 21:07:57 +0300

    test

--- srsCode/src/com/hillel/lesson_27/ex/Test4.java
+++ srsCode/src/com/hillel/lesson_27/ex/Test4.java
@@ -4,6 +4,9 @@ public class Test4 {
     public static void main(String[] args) {
         for (int i = 0; i < 1; System.out.println("Java123")) {
             System.out.println("test1");
+            if (true){
+                System.out.println("111");
+            }
             System.out.println("test3");
             System.out.println("test2");
         }
