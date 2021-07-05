6b03c96488eb54079c60edbaacc82ec38ab1f3b3
alex <stepurko.alexandr@gmail.com>
2021-07-05 21:07:18 +0300

    test commit

--- srsCode/src/com/hillel/lesson_27/ex/Test4.java
+++ srsCode/src/com/hillel/lesson_27/ex/Test4.java
@@ -2,8 +2,9 @@ package com.hillel.lesson_27.ex;
 
 public class Test4 {
     public static void main(String[] args) {
-        for (int i = 0; i < 1; System.out.println("Java1")) {
+        for (int i = 0; i < 1; System.out.println("Java123")) {
             System.out.println("test1");
+            System.out.println("test3");
             System.out.println("test2");
         }
     }
